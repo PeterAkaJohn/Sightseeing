044be0d94527600cf9e246d847ae6832a90dcbae
Pierpaolo Iannone <pierpaoloiannone@MacBook-Pro-di-Pierpaolo.local>
2017-04-21 21:25:24 +0200

    fixed issues with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: __dirname,
-    filename: './bundle.js'
+    filename: './app/bundle.js'
   },
   resolve: {
     modules: [path.resolve(__dirname, './app'), 'node_modules', 'bower_components'],
